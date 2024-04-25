c8eb27449a3bf82214852542240fa8af4e601cc1
Andreev Alexander <andreev.business.mail@gmail.com>
2024-04-25 23:35:58 +0300

    update

--- lab03/src/lab03.m
+++ lab03/src/lab03.m
@@ -1,7 +1,6 @@
 function lab03()
     clc();
 
-    debugFlg = 1;
     delayS = 0.8;
     a = 0;
     b = 1;
@@ -11,10 +10,15 @@ function lab03()
     hold on;
 
     pause(3);
-    parabolic_method(a, b, eps, debugFlg, delayS);
+
+    fprintf('-------Метод золотого сечения (поиск начальных точек x1, x2, x3)-------\n');
+    [x1, x2, x3, f1, f2, f3, a, b] = find_initial_points(a, b, eps, delayS);
+
+    fprintf('-------Параболический метод-------\n');
+    optimize_parabolic(x1, x2, x3, f1, f2, f3, a, b, eps, delayS);
 end
 
-function parabolic_method(a, b, eps, debugFlg, delayS)
+function [x1, x2, x3, f1, f2, f3, a, b] = find_initial_points(a, b, eps, delayS)
     tau = (sqrt(5)-1) / 2;
     l = b - a;
 
@@ -23,19 +27,22 @@ function parabolic_method(a, b, eps, debugFlg, delayS)
     f1 = f(x1);
     f2 = f(x2);
 
-    fprintf('-------Golden ratio method (looking for initial points x1, x2, x3)-------\n');
     i = 0;
-    if debugFlg
-        fprintf('№ %2d:\t [a, b] = [%.10f, %.10f], f(a) = %.10f, f(b) = %.10f\n', i, a, b, f(a), f(b));
-        line([a b], [f(a) f(b)], 'color', 'b');
-        hold on;
-    end
+
+    fprintf('№ %2d:\t [a, b] = [%.10f, %.10f], f(a) = %.10f, f(b) = %.10f\n', i, a, b, f(a), f(b));
+    line([a b], [f(a) f(b)], 'color', 'b');
+
+     i = 0;
+
+    fprintf('№ %2d:\t [a, b] = [%.10f, %.10f], f(a) = %.10f, f(b) = %.10f\n', i, a, b, f(a), f(b));
+    line([a b], [f(a) f(b)], 'color', 'b');
+
     while l > 2*eps
         i = i + 1;
-        if debugFlg
-            line([a b], [f(a) f(b)], 'color', 'b');
-            hold on;
-        end
+
+        line([a b], [f(a) f(b)], 'color', 'b');
+        hold on;
+
         if f1 <= f2
             b = x2;
             l = b - a;
@@ -68,38 +75,15 @@ function parabolic_method(a, b, eps, debugFlg, delayS)
             x1 = x2;        f1 = f2;
             x2 = new_x;     f2 = new_f;
         end
-        if debugFlg
-            fprintf('№ %2d:\t [a, b] = [%.10f, %.10f], f(a) = %.10f, f(b) = %.10f\n', i, a, b, f(a), f(b));
-            line([a b], [f(a) f(b)], 'color', 'r');
-            hold on;
-            pause(delayS);
-        end
-    end
 
-    if debugFlg
-        fprintf('Found x1 = %.10f, x2 = %.10f, x3 = %.10f\n', x1, x2, x3);
-        scatter(x1, f1, 'green', 'filled');
-        scatter(x2, f2, 'green', 'filled');
-        scatter(x3, f3, 'green', 'filled');
-        line([x1 x3], [f1 f3], 'color', 'b');
+        fprintf('№ %2d:\t [a, b] = [%.10f, %.10f], f(a) = %.10f, f(b) = %.10f\n', i, a, b, f(a), f(b));
+        line([a b], [f(a) f(b)], 'color', 'r');
         hold on;
-        pause(delayS*2);
+        pause(delayS);
     end
+end
 
-    if l <= 2*eps
-        x_res = (a+b)/2;
-        f_res = f(x_res);
-
-        if debugFlg
-            scatter(x_res, f_res, 'r', 'filled');
-            fprintf('RESULT: %2d iterations, x=%.10f, f(x)=%.10f\n', i, x_res, f_res);
-        end
-
-        return;
-    end
-
-    fprintf('-------Parabolic method-------\n');
-
+function optimize_parabolic(x1, x2, x3, f1, f2, f3, a, b, eps, delayS)
     a1 = (f2 - f1) / (x2 - x1);
     a2 = ((f3 - f1)/(x3 - x1) - (f2 - f1)/(x2 - x1)) / (x3 - x2);
     x_ = 1 / 2 * (x1 + x2 - a1/a2);
@@ -121,14 +105,12 @@ function parabolic_method(a, b, eps, debugFlg, delayS)
             x2 = x_; f2 = f_;
         end
 
-        if debugFlg
-            fprintf('№ %2d:\t [x1, x3] = [%.10f, %.10f], f(x1) = %.10f, f(x3) = %.10f\n', i, x1, x3, f1, f3);
-            fprintf('Current min point: x=%.10f, f(x)=%.10f\n', x_, f_);
-            line([x1 x3], [f1 f3], 'color', 'b');
-            plot(x_, f_, 'xk');
-            hold on;
-            pause(delayS);
-        end
+        fprintf('№ %2d:\t [x1, x3] = [%.10f, %.10f], f(x1) = %.10f, f(x3) = %.10f\n', i, x1, x3, f1, f3);
+        fprintf('Current min point: x=%.10f, f(x)=%.10f\n', x_, f_);
+        line([x1 x3], [f1 f3], 'color', 'b');
+        plot(x_, f_, 'xk');
+        hold on;
+        pause(delayS);
 
         a1 = (f2 - f1) / (x2 - x1);
         a2 = ((f3 - f1)/(x3 - x1) - (f2 - f1)/(x2 - x1)) / (x3 - x2);
@@ -143,14 +125,11 @@ function parabolic_method(a, b, eps, debugFlg, delayS)
     x_res = x_;
     f_res = f_;
 
-    if debugFlg
-        scatter(x_res, f_res, 'r', 'filled');
-        fprintf('RESULT: %2d iterations, x=%.10f, f(x)=%.10f\n', i, x_res, f_res);
-    end
-
+    scatter(x_res, f_res, 'r', 'filled');
+    fprintf('РЕЗУЛЬТАТ: %2d итераций, x=%.10f, f(x)=%.10f\n', i, x_res, f_res);
 end
 
 function y = f(x)
-    %y = cos(power(x,5) - x + 3 + power(2, 1/3)) + atan((power(x,3) - 5 * sqrt(2)*x - 4) / (sqrt(6)*x + sqrt(3))) + 1.8;
+    %y = exp(((x^4) + (x^2) - x + sqrt(5)) / 5) + sinh((x^3 + 21 * x + 9) / (21*x + 6)) - 3.0;
     y = (x-0.222).^4;
 end
